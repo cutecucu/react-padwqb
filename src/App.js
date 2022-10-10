1e94ea07e58bbe2f81bda1c2c004e11db74cb320
cutecucu <75126593+cutecucu@users.noreply.github.com>
2022-10-10 15:56:10 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./style.css";
+function Header(){
+  return <h1>메이크메이트:)</h1>
+}
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a>html</a></li>
+      <li><a>css</a></li>
+      <li><a>js</a></li>
+    </ol>
+  </nav>
+}
 
+function Article(){
+  return <article>
+    <h2>Welcom</h2>
+    Hello, WEB
+  </article>
+}
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
